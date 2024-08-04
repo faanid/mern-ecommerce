35bc84962a084eec2adaf872ba8cca1f61cd07e1
faanid <73032767+faanid@users.noreply.github.com>
2024-08-04 19:32:09 +0330

    py-alg

---
+++ sticky-navigation/grok.py
@@ -0,0 +1,9 @@
+# def look_for_keymain_box(:
+#     pile = main_box.make_a_pile_to_look_through)(
+# While pile is not empty :
+# box = pile.grab_a_box)(
+# for item in box:
+# if item.is_a_box)(:
+# pile.append)item(
+# elif item.is_a_key)(:
+# print “found the key“!
