dc092108099b4a0e6d0289c9d80cd4a268c99ac5
faanid <73032767+faanid@users.noreply.github.com>
2024-08-09 22:06:36 +0330

    Custom Error Handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,16 +11,18 @@ router.get(
   })
 );
 
-// router.get(
-//   "/:id",
-//   asyncHandler(async (req, res) => {
-//     const product = await Product.findById(req.params.id);
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-//     if (product) {
-//       return res.json(product);
-//     }
-//     res.status(404).json({ message: "Product not found" });
-//   })
-// );
+    if (product) {
+      return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Resource not found");
+    }
+  })
+);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
@@ -15,5 +16,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/product", productRoutes); //anytime we hit this route  ->  '/api/product'  it's gonna go to this file -> productRoutes
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
