0d3590707e929be3ffa2546064d330de17f42fc2
faanid <73032767+faanid@users.noreply.github.com>
2024-08-10 19:57:50 +0330

    Product Controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,33 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler";
-import Product from "../modules/productModel";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+// router.get(
+//   "/",
+//   asyncHandler(async (req, res) => {
+//     const products = await Product.find({});
+//     res.json(products);
+//   })
+// );
 
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Resource not found");
-    }
-  })
-);
+// router.get(
+//   "/:id",
+//   asyncHandler(async (req, res) => {
+//     const product = await Product.findById(req.params.id);
+
+//     if (product) {
+//       return res.json(product);
+//     } else {
+//       res.status(404);
+//       throw new Error("Resource not found");
+//     }
+//   })
+// );
 
 export default router;
