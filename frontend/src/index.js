a3992fabdbf74abc8b853da4a977bf1636dd3745
faanid <73032767+faanid@users.noreply.github.com>
2024-08-15 19:56:07 +0330

    Redux Store & Api Slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import {
   RouterProvider,
 } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./assets/styles/index.css";
 import "./assets/styles/bootstrap.custom.css";
 import App from "./App";
@@ -26,7 +28,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
